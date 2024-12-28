cf981e618ba996a1de2654603f5e005c0bc9ee54
Lilithe Bowman <lilithe.bowman@gmail.com>
2024-12-28 17:54:57 -0500

    Sparkles when Chest is Opened

--- Assets/Eternal Temple/Scripts/Udon#/LilChestOpen.cs
+++ Assets/Eternal Temple/Scripts/Udon#/LilChestOpen.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-public class LilChestOpen : UdonSharpBehaviour
-{
-    void Start()
-    {
-        
-    }
+public class LilChestOpen : UdonSharpBehaviour {
+	public Animator animator;
+	public string openingAnimation = "Chest_01_Opening";
+	public string closingAnimation = "Chest_01_Closing";
+	public bool isOpen;
+
+	void Start () {
+		if (animator == null) {
+			Debug.LogError("No animator detected! Set up the proper animator!");
+		}
+		if (openingAnimation == null || closingAnimation == null) {
+			Debug.LogError("No animator name is set! We can't start an animation we don't know the name of.");
+		}
+	}
+
+	public override void Interact () {
+		if (animator != null && openingAnimation != null && closingAnimation != null) {
+			if (!isOpen) {
+				animator.Play(openingAnimation);
+				isOpen = true;
+			} else {
+				animator.Play(closingAnimation);
+				isOpen = false;
+			}
+		}
+	}
 }
