896149278435d2283c02d1102e712c8c690cfebf
Lilithe Bowman <lilithe.bowman@gmail.com>
2025-01-06 15:53:31 -0500

    Made a cave for the user to teleport

---
+++ Assets/Scripts/TeleportToCave.cs
@@ -0,0 +1,16 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TeleportToCave : UdonSharpBehaviour {
+	public Transform teleportTo;
+
+	public void OnPlayerTriggerEnter () {
+		VRCPlayerApi playerApi;
+		playerApi = Networking.LocalPlayer;
+		playerApi.TeleportTo(teleportTo.position, Quaternion.identity * teleportTo.rotation);
+	}
+
+}
